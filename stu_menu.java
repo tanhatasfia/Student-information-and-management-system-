051895b6eebef718b88a448cc8f891e5fde134c9
tanhatasfia <66655911+tanhatasfia@users.noreply.github.com>
2023-08-01 13:12:36 +0600

    Delete stu_menu.java

--- stu_menu.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class stu_menu {
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
-}
